7eb297d1084bd52f40c0aa50ca5254c94cfdcbfe
fisham33 <fisham33@gmail.com>
2025-11-20 00:41:38 +1100

    simplify file structure

--- src/data/dungeon_definitions.h
+++ src/data/dungeon_definitions.h
@@ -21,13 +21,6 @@
 
 static const struct Dungeon sDungeons[DUNGEON_COUNT] = {
     [DUNGEON_EARLY_CAVE] = {
-        .id = DUNGEON_EARLY_CAVE,
-        .tier = DUNGEON_TIER_EARLY,
-        .biome = DUNGEON_BIOME_CAVE,
-        .baseLevel = 20,
-        .levelRange = 5,  // Level range: 15-25
-        .roomCount = 5,
-        .entranceMap = MAP_UNDEFINED, // TODO: Replace with MAP_DUNGEON_CAVE_ENTRANCE
         .roomPool = sCaveRooms,
         .roomPoolSize = ARRAY_COUNT(sCaveRooms),
 
@@ -39,13 +32,6 @@ static const struct Dungeon sDungeons[DUNGEON_COUNT] = {
         .fixedModifierId = DUNGEON_DEFAULT_FIXED_MODIFIER_ID,
     },
     [DUNGEON_MID_FOREST] = {
-        .id = DUNGEON_MID_FOREST,
-        .tier = DUNGEON_TIER_MID,
-        .biome = DUNGEON_BIOME_FOREST,
-        .baseLevel = 40,
-        .levelRange = 5,  // Level range: 35-45
-        .roomCount = 5,
-        .entranceMap = MAP_UNDEFINED, // TODO: Replace with MAP_DUNGEON_FOREST_ENTRANCE
         .roomPool = sForestRooms,
         .roomPoolSize = ARRAY_COUNT(sForestRooms),
 
@@ -57,13 +43,6 @@ static const struct Dungeon sDungeons[DUNGEON_COUNT] = {
         .fixedModifierId = DUNGEON_DEFAULT_FIXED_MODIFIER_ID,
     },
     [DUNGEON_LATE_MOUNTAIN] = {
-        .id = DUNGEON_LATE_MOUNTAIN,
-        .tier = DUNGEON_TIER_LATE,
-        .biome = DUNGEON_BIOME_MOUNTAIN,
-        .baseLevel = 60,
-        .levelRange = 5,  // Level range: 55-65
-        .roomCount = 5,
-        .entranceMap = MAP_UNDEFINED, // TODO: Replace with MAP_DUNGEON_MOUNTAIN_ENTRANCE
         .roomPool = sMountainRooms,
         .roomPoolSize = ARRAY_COUNT(sMountainRooms),
 
